e1401fe4bf353ac0513a2a16cc88650820d81e66
kerizalperwastaken <105068896+kerizalperwastaken@users.noreply.github.com>
2023-08-01 10:18:40 +0000

    hi

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   prefix: "?", // Your Bot Prefix
   client_id: "1135834067368149143", // Your Application Client ID
   redirect_uri: "https://verify.k3riz.repl.co/auth", // Your Application Redirect Url
-  oauth_link: "https://discord.com/oauth2/authorize?client_id=1135665819846713384&redirect_uri=https://verify.k3riz.repl.co/auth&response_type=code&scope=identify%20guilds.join", // Your Application Auth Link (guild.joins + identify)
+  oauth_link: "https://discord.com/api/oauth2/authorize?client_id=1135834067368149143&redirect_uri=https%3A%2F%2Fverify.k3riz.repl.co%2Fauth&response_type=code&scope=guilds.join%20identify", // Your Application Auth Link (guild.joins + identify)
   owners: ["1125797410690564187"],
   bot_link: "https://discord.com/api/oauth2/authorize?client_id=1135665819846713384&permissions=8&scope=bot%20applications.commands",
 
